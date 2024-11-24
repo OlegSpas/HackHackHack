8f89ab7f645e36022e823f2fb473d11fc962511b
Oleh-Bohdan Spas <oleh-bohdan.spas@student.tuke.sk>
2024-11-24 12:03:28 +0100

    'fix'

--- front/src/components/pages/LoadingPage.jsx
+++ front/src/components/pages/LoadingPage.jsx
@@ -3,27 +3,18 @@ import '../styles/StyleLoadingPage.scss';
 import { Link, useNavigate } from 'react-router-dom';
 
 function LoadingPage() {
+
     const navigate = useNavigate();
 
     useEffect(() => {
-        const checkForSourceId = async () => {
-            try {
-                const response = await fetch('/results.json');
-                if (response.ok) {
-                    const data = await response.json();
-                    if (JSON.stringify(data).includes('sourceid')) {
-                        navigate('/vulnerabilities'); // Перенаправляємо на іншу сторінку
-                    }
-                }
-            } catch (error) {
-                console.error('Error fetching results.json:', error);
-            }
-        };
+        const timer = setTimeout(() => {
+            navigate('/vulnerabilities');
+        }, 25000);
 
-        const interval = setInterval(checkForSourceId, 3000); // Перевіряємо кожні 3 секунди
-
-        return () => clearInterval(interval); // Очищаємо інтервал при демонтажі компонента
+        return () => clearTimeout(timer);
     }, [navigate]);
+
+    //TODO if you click on Link it scanning will be continue
 
     return (
         <section className="LoadingPage">
@@ -31,15 +22,13 @@ function LoadingPage() {
                 <div className="LoadingPage__content">
                     <div className="LoadingPage__main_content">
                         <header className="LoadingPage__header">
-                            <Link className="to_load_btn" to="/">
+                            <Link className='to_load_btn' to="/">
                                 <button className="back__button">
                                     Back
                                 </button>
                             </Link>
-                            <Link className="Logo__btn" to="/">
-                                <h1 className="LoadingPage__logo">
-                                    <span className="logo__purple">Web</span>Check
-                                </h1>
+                            <Link className='Logo__btn' to="/">
+                                <h1 className='LoadingPage__logo'><span className='logo__purple'>Web</span>Check</h1>
                             </Link>
                         </header>
                         <div className="loading">
@@ -47,21 +36,19 @@ function LoadingPage() {
                                 It will take some time. Please wait...
                             </h2>
                             <div className="loadingPage__main_loading">
-                                <div className="progress-bar">
-                                    <div className="bar"></div>
+                                <div class="progress-bar">
+                                    <div class="bar"></div>
                                 </div>
                             </div>
                         </div>
-                        <div className="copyright">
-                            <h3 className="copyright__text__dark">
-                                MADE BY <span className="team_name">IPSO</span> TEAM
-                            </h3>
-                        </div>
+                        <div className="copyright"><h3 className='copyright__text__dark'>MADE BY <span className='team_name'>IPSO</span> TEAM</h3></div>
                     </div>
                 </div>
             </div>
         </section>
-    );
+    )
+
+
 }
 
 export default LoadingPage;

5a443618f3a4185bdfbad475bcc427e02a4994c0
Oleh-Bohdan Spas <oleh-bohdan.spas@student.tuke.sk>
2024-11-24 08:39:00 +0100

    fix front

--- front/src/components/pages/LoadingPage.jsx
+++ front/src/components/pages/LoadingPage.jsx
@@ -9,19 +9,23 @@ function LoadingPage() {
             <div className="container">
                 <div className="LoadingPage__content">
                     <div className="LoadingPage__main_content">
-                    <header className="LoadingPage__header">
-                        <button className="back__button">
-                            Back
-                        </button>
-                        <h1 className='LoadingPage__logo'><span className='logo__purple'>Web</span>Check</h1>
-                    </header>
-                    <div className="loading">
-                        <h2 className="loading__description">
-                            It will take some time. Please wait...
-                        </h2>
-                        <div className="loadingPage__main_loading"></div>
-                    </div>
-                    <div className="copyright"><h3 className='copyright__text__dark'>MADE BY <span className='team_name'>IPSO</span> TEAM</h3></div>
+                        <header className="LoadingPage__header">
+                            <button className="back__button">
+                                Back
+                            </button>
+                            <h1 className='LoadingPage__logo'><span className='logo__purple'>Web</span>Check</h1>
+                        </header>
+                        <div className="loading">
+                            <h2 className="loading__description">
+                                It will take some time. Please wait...
+                            </h2>
+                            <div className="loadingPage__main_loading">
+                                <div class="progress-bar">
+                                    <div class="bar"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="copyright"><h3 className='copyright__text__dark'>MADE BY <span className='team_name'>IPSO</span> TEAM</h3></div>
                     </div>
                 </div>
             </div>
